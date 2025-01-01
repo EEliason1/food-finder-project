f468729007c2224e3cb14073751370a35a94b97f
Evan Eliason <evaneliason1@gmail.com>
2025-01-01 14:18:56 -0700

    add simple components to test tailwind, fix postcss to ensure styling works

---
+++ frontend/src/components/Navbar.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Navbar: React.FC = () => {
+  return (
+    <div className="flex">
+      <h1 className="text-slate-500">Navbar</h1>
+      <h2>Test</h2>
+    </div>
+  );
+};
+
+export default Navbar;
